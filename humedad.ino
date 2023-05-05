155f4003ddb44e127da30190f329968ccdb84438
xVrzBx <bryancs4899@gmail.com>
2023-05-05 15:06:37 -0600

    Creating files for start working

---
+++ humedad.ino
@@ -0,0 +1,11 @@
+//@version. 1.0.0 
+//Last to modify: @bryan 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ solar.ino
@@ -0,0 +1,14 @@
+//@version. 1.0.0 
+//Last to modify: @bryan 
+/**Descripción: 
+ * Archivo para programar el seguimiento del servomotor por medio de sensores de luz 
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
