74cb111bb00318fba4acf2ac26f340cc4bbf2d83
xVrzBx <bryancs4899@gmail.com>
2023-05-05 19:55:35 -0600

    Add reading of humety sensor

---
+++ bomba.ino
@@ -0,0 +1,16 @@
+//@version. 1.0.0 
+//Last to modify: @bryan 
+
+/**Descripción: 
+ * Archivo para programar la bomba 
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- humedad.ino
+++ humedad.ino
@@ -1,11 +1,20 @@
 //@version. 1.0.0 
 //Last to modify: @bryan 
+/**Descripción: 
+ * Archivo para programar el sensor de humedad
+ * 
+*/
+const int sensorPin = A0;
 void setup() {
-  // put your setup code here, to run once:
-
+  //Iniciar comunicacion serial entre arduino y pc 9600 es la velocidad de
+  //transmisión de datos a través del puerto serie. (9600 baudios)
+  Serial.begin(9600);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //
+  int humedad = analogRead(sensorPin);
+  Serial.println(humedad);
+  delay(1000);
+  
+  
 }
