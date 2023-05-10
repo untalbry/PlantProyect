207d91dda4b2510a4a27b89e56649ea3a644333b
RommelG <diegorommelg@gmail.com>
2023-05-09 21:15:13 -0600

    Add percentage for moisture measurement

--- humedad/humedad.ino
+++ humedad/humedad.ino
@@ -1,24 +1,30 @@
-//@version. 1.0.0 
-//Last to modify: @bryan 
+//@version. 1.0.1
+//Last to modify: @rommel
 /**Descripción: 
  * Archivo para programar el sensor de humedad
  * 
+ * Modificaciones: Agregar función map para el porcentaje de humedad
 */
+
 const int sensorPin = A0;
-const int HUMEDAD_MINIMA = 500;
+const int HUMEDAD_MINIMA = 60; //valor máximo
 void setup() {
   //Iniciar comunicacion serial entre arduino y pc 9600 es la velocidad de
   //transmisión de datos a través del puerto serie. (9600 baudios)
   Serial.begin(9600);
 }
 void loop() {
-  //se crea va
-  int humedad = analogRead(sensorPin);
-  Serial.println(humedad);
+  //se crea variable de humedad y de porcentaje
+  int humedad = map(analogRead(sensorPin), 1023, 0, 0, 100);
+  Serial.print("humedad");
+  Serial.print(humedad);
+  Serial.println("%");
+  Serial.print();
   if(humedad < HUMEDAD_MINIMA){
     Serial.println("Regando planta");
     //llamar a función de bomba
   }
+
   delay(1000);
   
   
