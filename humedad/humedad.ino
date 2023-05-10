7d1281e3ad23da83e8b2f6454c03df06ba87b538
Bryan Castro <91161604+xVrzBx@users.noreply.github.com>
2023-05-10 09:19:20 -0600

    Fixed humety and deleted file bomba

--- humedad/humedad.ino
+++ humedad/humedad.ino
@@ -1,5 +1,5 @@
-//@version. 1.0.1
-//Last to modify: @rommel
+//@version. 1.0.2
+//Last to modify: @bryan
 /**Descripción: 
  * Archivo para programar el sensor de humedad
  * 
@@ -14,17 +14,24 @@ void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  //se crea variable de humedad y de porcentaje
-  int humedad = map(analogRead(sensorPin), 1023, 0, 0, 100);
-  Serial.print("humedad");
+  //se crea variable de conductivilidad que recibe los valores del sensor 
+  int conductivilidad = analogRead(sensorPin);
+  //Convertimos la variable anterior a un porcentaje de humedad 
+  //Donde 0 es el valor máximo de humedad y 1024 el valor mínimo
+  int humedad = map( conductivilidad, 1024, 0, 0, 100);
+  
+  Serial.print("Conductivilidad 0-1024 : ");
+  Serial.print(conductivilidad);
+  Serial.print(" humedad: ");
   Serial.print(humedad);
   Serial.println("%");
-  Serial.print();
+  //Si la humedad es menor al que debe de tener la planta en cuestión
+  //Se prende la bomba para regar la planta. 
   if(humedad < HUMEDAD_MINIMA){
     Serial.println("Regando planta");
     //llamar a función de bomba
   }
-
+  
   delay(1000);
   
   
